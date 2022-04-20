3e756dd55fc45fa4daee15ac44ee3e31b654f375
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-20 00:34:23 +0000

    Changed carousel icons and some css.

--- js/data.js
+++ js/data.js
@@ -1,6 +1,3 @@
-/* exported getCircuitData */
-
-const $circuitInfoContainer = document.querySelector('.circuit-info-container');
 
 function getCircuitData(circuitId) {
   fetch(`http://ergast.com/api/f1/current/${circuitId}.json`)
@@ -22,73 +19,4 @@ function getDriverStandings(year) {
     .then(data => {
       renderSmallDriverStandings(data);
     });
-}
-
-// Devo mettere on window load !!!!!!!!!!!!!!!!!!
-getConstructorStandings(2022);
-getDriverStandings(2022);
-
-function renderCircuitData(data) {
-  const $circuitInfoDiv = document.querySelector('.circuit-info');
-  const html = `
-  <div class="circuit-info">
-              <h1>${data.MRData.RaceTable.Races[0].raceName}</h1>
-              <p><strong>Circuit</strong>: ${data.MRData.RaceTable.Races[0].Circuit.circuitName}</p>
-              <p><strong>Date</strong>: ${data.MRData.RaceTable.Races[0].date}</p>
-              <p><strong>Time</strong>: ${data.MRData.RaceTable.Races[0].time}</p>
-              <a class="wiki-btn home-btn blue-bkgd" href="${data.MRData.RaceTable.Races[0].Circuit.url}">Wiki Report</a>
-            </div>
-  `;
-  $circuitInfoDiv.remove();
-  $circuitInfoContainer.insertAdjacentHTML('afterbegin', html);
-}
-
-function renderSmallConstructorStandings(data) {
-  const $constructorsTbody = document.querySelector(
-    '.constructors-standings-tbody'
-  );
-
-  for (let i = 0; i < 5; i++) {
-    const $tr = document.createElement('tr');
-    const $td1 = document.createElement('td');
-    $td1.textContent = i + 1;
-    const $td2 = document.createElement('td');
-    $td2.textContent =
-      data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[
-        i
-      ].Constructor.name;
-    const $td3 = document.createElement('td');
-    $td3.textContent =
-      data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[
-        i
-      ].points;
-    $tr.appendChild($td1);
-    $tr.appendChild($td2);
-    $tr.appendChild($td3);
-    $constructorsTbody.appendChild($tr);
-  }
-}
-
-function renderSmallDriverStandings(data) {
-  const $driversTbody = document.querySelector('.drivers-standings-tbody');
-
-  for (let i = 0; i < 5; i++) {
-    const $tr = document.createElement('tr');
-    const $td1 = document.createElement('td');
-    $td1.textContent = i + 1;
-    const $td2 = document.createElement('td');
-    $td2.textContent =
-      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
-        .givenName +
-      ' ' +
-      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
-        .familyName;
-    const $td3 = document.createElement('td');
-    $td3.textContent =
-      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
-    $tr.appendChild($td1);
-    $tr.appendChild($td2);
-    $tr.appendChild($td3);
-    $driversTbody.appendChild($tr);
-  }
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-global-assign */
-/* global getCircuitData */
-/* exported getCircuitData */
+
 
 var $leftArrow = document.querySelector('.left-arrow');
 var $rightArrow = document.querySelector('.right-arrow');
 var $image = document.querySelector('.carousel-image');
-var $progressDots = document.querySelectorAll('.dot');
+var $progressDots = document.querySelectorAll('.ph-circle');
 var $dotsContainer = document.querySelector('.progress-dots');
+const $circuitInfoContainer = document.querySelector('.circuit-info-container');
 
 var curSlide = 1;
 var maxSlide = $progressDots.length;
@@ -22,10 +22,12 @@ var maxSlide = $progressDots.length;
 
 window.addEventListener('DOMContentLoaded', event => {
   getCircuitData(1);
+  getConstructorStandings(2022);
+  getDriverStandings(2022);
 });
 
 $dotsContainer.addEventListener('click', function (event) {
-  if (event.target.nodeName === 'BUTTON') {
+  if (event.target.nodeName === 'I') {
     curSlide = parseInt(event.target.getAttribute('id'));
     selectDot(curSlide);
     renderImage(curSlide);
@@ -35,9 +37,9 @@ $dotsContainer.addEventListener('click', function (event) {
 
 function selectDot(id) {
   for (var i = 0; i < $progressDots.length; i++) {
-    $progressDots[i].className = 'dot';
+    $progressDots[i].className = 'ph-circle';
   }
-  $progressDots[id - 1].classList.add('selected');
+  $progressDots[id - 1].classList.add('ph-circle-fill');
   getCircuitData(id);
 }
 
@@ -69,5 +71,70 @@ function goPreviousImg() {
     curSlide--;
     renderImage(curSlide);
     selectDot(curSlide);
+  }
+}
+
+function renderCircuitData(data) {
+  const $circuitInfoDiv = document.querySelector('.circuit-info');
+  const html = `
+  <div class="circuit-info">
+              <h1>${data.MRData.RaceTable.Races[0].raceName}</h1>
+              <p><strong>Circuit</strong>: ${data.MRData.RaceTable.Races[0].Circuit.circuitName}</p>
+              <p><strong>Date</strong>: ${data.MRData.RaceTable.Races[0].date}</p>
+              <p><strong>Time</strong>: ${data.MRData.RaceTable.Races[0].time}</p>
+              <a class="wiki-btn home-btn blue-bkgd" href="${data.MRData.RaceTable.Races[0].Circuit.url}" target="_blank">Wiki Report</a>
+            </div>
+  `;
+  $circuitInfoDiv.remove();
+  $circuitInfoContainer.insertAdjacentHTML('afterbegin', html);
+}
+
+function renderSmallConstructorStandings(data) {
+  const $constructorsTbody = document.querySelector(
+    '.constructors-standings-tbody'
+  );
+
+  for (let i = 0; i < 5; i++) {
+    const $tr = document.createElement('tr');
+    const $td1 = document.createElement('td');
+    $td1.textContent = i + 1;
+    const $td2 = document.createElement('td');
+    $td2.textContent =
+      data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[
+        i
+      ].Constructor.name;
+    const $td3 = document.createElement('td');
+    $td3.textContent =
+      data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[
+        i
+      ].points;
+    $tr.appendChild($td1);
+    $tr.appendChild($td2);
+    $tr.appendChild($td3);
+    $constructorsTbody.appendChild($tr);
+  }
+}
+
+function renderSmallDriverStandings(data) {
+  const $driversTbody = document.querySelector('.drivers-standings-tbody');
+
+  for (let i = 0; i < 5; i++) {
+    const $tr = document.createElement('tr');
+    const $td1 = document.createElement('td');
+    $td1.textContent = i + 1;
+    const $td2 = document.createElement('td');
+    $td2.textContent =
+      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
+        .givenName +
+      ' ' +
+      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
+        .familyName;
+    const $td3 = document.createElement('td');
+    $td3.textContent =
+      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
+    $tr.appendChild($td1);
+    $tr.appendChild($td2);
+    $tr.appendChild($td3);
+    $driversTbody.appendChild($tr);
   }
 }
