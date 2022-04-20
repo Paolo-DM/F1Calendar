a75543ef459ecd8f2e6319737819d659316bd5f9
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-20 18:36:56 +0000

    Added drivers'images and the functionality to render drivers'standing info from the API.

--- js/data.js
+++ js/data.js
@@ -16,6 +16,90 @@ function getDriverStandings(year) {
   fetch(`http://ergast.com/api/f1/${year}/driverStandings.json`)
     .then((response) => response.json())
     .then((data) => {
-      renderSmallDriverStandings(data);
+      renderFullllDriverStandings(data);
+      // renderSmallDriverStandings(data);
     });
+}
+
+function renderFullllDriverStandings(data) {
+  const $driversFullTbody = document.querySelector(
+    ".tbody-full-driver-standings"
+  );
+
+  /*
+<tr>
+              <td>1</td>
+              <td>
+                <span
+                  ><img
+                    class="pilot-icon"
+                    src="images/pilots/leclerc.png" /></span
+                >&nbsp;Charles Leclerc
+              </td>
+              <td>
+                <span
+                  ><img
+                    class="pilot-nationality-icon"
+                    src="images/monaco-2.png" /></span
+                >&nbsp;Monegasque
+              </td>
+              <td>Ferrari</td>
+              <td>2</td>
+              <td>71</td>
+            </tr>
+*/
+
+  for (let i = 0; i < data.MRData.total; i++) {
+    const html = `
+      <tr>
+        <td>${i + 1}</td>
+        <td>
+          <span>
+            <img class="pilot-icon" src="images/pilots/${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i]
+        .Driver.familyName
+      }.png" />
+          </span>
+          &nbsp;${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i]
+        .Driver.givenName +
+      " " +
+      data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i]
+        .Driver.familyName
+      }
+        </td>
+        <td>
+          <span>
+            <img class="pilot-nationality-icon" src="images/monaco-2.png" />
+          </span>
+          &nbsp;${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i]
+        .Driver.nationality
+      }
+        </td>
+        <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i]
+        .Constructors[0].name
+      }</td>
+        <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].wins
+      }</td>
+        <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points
+      }</td>
+      </tr>
+    `;
+    // let $tr = document.createElement("tr");
+    // let $td1 = document.createElement("td");
+    // $td1.textContent = i + 1;
+    // let $td2 = document.createElement("td");
+    // $td2.textContent =
+    //   data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
+    //     .givenName +
+    //   " " +
+    //   data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver
+    //     .familyName;
+    // let $td3 = document.createElement("td");
+    // $td3.textContent =
+    //   data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
+    // $tr.appendChild($td1);
+    // $tr.appendChild($td2);
+    // $tr.appendChild($td3);
+    // $driversTbody.appendChild($tr);
+    $driversFullTbody.insertAdjacentHTML("beforeend", html);
+  }
 }
